48b9d6a114d8a4d7ed39ea2dec5db3eda8ceaf79
isaac musuayi <isaacmusuayi216@gmail.com>
2022-11-09 17:38:42 +0100

    feat: ajout de la fonctionnalité remove

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import { menuArray } from "./data.js";
 
 document.addEventListener("click", function (e) {
-  handleChoiceClick(parseInt(e.target.dataset.add));
+  if (e.target.dataset.add) {
+    handleChoiceClick(parseInt(e.target.dataset.add));
+  } else if (e.target.dataset.remove) {
+    removeItem(e.target.dataset.remove, e.target);
+  }
 
-  if(e.target.dataset.add) {
-    document.getElementById('your-order-container').classList.add('open')
+  if (e.target.dataset.add) {
+    document.getElementById("your-order-container").classList.add("open");
   }
 });
 
@@ -14,13 +18,22 @@ function handleChoiceClick(foodId) {
   })[0];
 
   document.getElementById("container-item").innerHTML += `
-  <div class="items-div">
-  <div class="item" id="item">${targetFoodObj.name}<span>remove</span></div>
-  <div class="item-price">${targetFoodObj.price}</div>
+  <div class="items-div" id="items-div">
+    <div class="item" id="item1">${targetFoodObj.name}<span id="removeIre" data-remove="${targetFoodObj.id}">remove</span></div>
+    <div class="item-price">${targetFoodObj.price}</div>
   </div>
   `;
-
   sumUp(targetFoodObj.price);
+}
+
+function removeItem(itemId, span) {
+  console.log("remove ", itemId);
+  const grandParent = span.parentNode.parentNode;
+  grandParent.remove();
+  if (document.querySelectorAll(".items-div").length === 0) {
+    document.getElementById("your-order-container").classList.remove("open");
+  }
+  //   document.getElementById(`item-${itemId}`).classList.add("hidden");
 }
 
 let total = 0;
