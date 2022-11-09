f8a1cabc360527f55875655228526b0b30ca7ab6
isaac musuayi <isaacmusuayi216@gmail.com>
2022-11-09 11:41:01 +0100

    feat : l'affichage de la commande et leur prix total

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 import { menuArray } from "./data.js";
 
+document.addEventListener("click", function (e) {
+  handleChoiceClick(parseInt(e.target.dataset.add));
+
+  if(e.target.dataset.add) {
+    document.getElementById('your-order-container').classList.add('open')
+  }
+});
+
+function handleChoiceClick(foodId) {
+  const targetFoodObj = menuArray.filter(function (food) {
+    return food.id === foodId;
+  })[0];
+
+  document.getElementById("container-item").innerHTML += `
+  <div class="items-div">
+  <div class="item" id="item">${targetFoodObj.name}<span>remove</span></div>
+  <div class="item-price">${targetFoodObj.price}</div>
+  </div>
+  `;
+
+  sumUp(targetFoodObj.price);
+}
+
+let total = 0;
+
+function sumUp(sum) {
+  total = total + sum;
+
+  document.getElementById("total-price").innerText = total;
+}
+
 function getMenu() {
   let menuHtml = ``;
 
@@ -15,8 +46,8 @@ function getMenu() {
           <small class="ingredients">${feed.ingredients}</small>
           <div class="price">${feed.price}</div>
         </div>
-        <div class="container--add-btn">
-            <img src="images/Text.png" alt="">
+        <div class="container--add-btn" data-add="${feed.id}">
+           
         </div>
 
       </div>
@@ -29,7 +60,7 @@ function getMenu() {
 }
 
 function render() {
-    document.getElementById('container').innerHTML = getMenu()
+  document.getElementById("container").innerHTML = getMenu();
 }
 
-render()
+render();
